d250374cfbcbb2a082c2ddb54cd3c57dc6367de4
onyedikachi okonkwo <okonkwovincent@gmail.com>
2024-05-29 02:02:51 +0100

    added

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -19,7 +19,7 @@ export default Register
 const Cover = styled.div`
 width: 100%;
 height: 100vh;
-background-color: rgba(0,0,0,0.4);
+background-color: rgba(0,0,0,0.3);
 `
 
 const Right = styled.div`
