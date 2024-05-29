e8823eabf1581b2b179807b5e08f323db5eea3fc
onyedikachi okonkwo <okonkwovincent@gmail.com>
2024-05-29 01:53:52 +0100

    added

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,8 +1,34 @@
+import styled from "styled-components"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <Container>
+        <Left></Left>
+        <Right></Right>
+      </Container>
+    </>
   )
 }
 
 export default Register
+
+const Right = styled.div`
+width: 50%;
+height: 100vh;
+background-color:red;
+`
+
+const Left = styled.div`
+width: 50%;
+height: 100vh;
+background: #000;
+`
+
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+align-items: center;
+justify-content: center;
+`
