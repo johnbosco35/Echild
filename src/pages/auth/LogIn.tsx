9676f8473951e0a2359f254afedd0d270d466bb4
onyedikachi okonkwo <okonkwovincent@gmail.com>
2024-05-29 04:28:18 +0100

    added

--- src/pages/auth/LogIn.tsx
+++ src/pages/auth/LogIn.tsx
@@ -1,8 +1,95 @@
+import styled from "styled-components"
+import bg from "../../assets/image-3.jpg"
+import { NavLink } from "react-router-dom"
 
 const LogIn = () => {
   return (
-    <div>LogIn</div>
+    <>
+      <Container>
+          <Cover>
+          <h1>Register your child</h1>
+          <Box>
+            <p>Full Name:</p>
+            <input type="text" placeholder="Username" required/>
+          </Box>
+          <Box>
+            <p>Email:</p>
+            <input type="text" placeholder="example@gmail.com" required/>
+          </Box>
+          <Box>
+            <p>Password:</p>
+            <input type="text" placeholder="password" required/>
+          </Box>
+          <Button>Register</Button>
+          <div style={{display:"flex",alignItems:"center",justifyContent:"center",fontSize:"20px",fontWeight:"800",marginTop:"15px"}}>
+            <span>Already have an account?</span>
+            <NavLink to={"/login"} style={{textDecoration:"none",color:"white",marginLeft:"10px"}}>Signin</NavLink>
+          </div>
+          </Cover>
+      </Container>
+    </>
   )
 }
 
 export default LogIn
+
+const Button = styled.button`
+  width: 160px;
+  height: 40px;
+  border-radius: 5px;
+  margin-top: 10px;
+  border: none;
+  cursor: pointer;
+  margin-left: 250px;
+  font-size: 20px;
+  font-weight: 800;
+  font-family: Comic Sans MS;
+
+  &:hover{
+    background-color: red;
+    color: white;
+  }
+`
+
+const Box = styled.div`
+margin-left: 20px;
+
+input{
+  outline: none;
+  width: 380px;
+  height: 30px;
+  padding-left: 5px;
+  border-radius: 5px;
+  border: none;
+}
+
+p{
+  margin: 0;
+  font-size: 20px;
+  font-weight: 800;
+  font-family: Comic Sans MS;
+}
+`
+
+const Cover = styled.div`
+width: 100%;
+height: 100vh;
+background-color: rgba(0,0,0,0.3);
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+color: white;
+`
+
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+background-image:url(${bg});
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+display: flex;
+align-items: center;
+justify-content: center;
+`
