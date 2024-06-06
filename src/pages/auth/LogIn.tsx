7c2b26ec1d79d967a91e823e38ca228e3b8c4b62
onyedikachi okonkwo <okonkwovincent@gmail.com>
2024-06-06 01:34:01 +0100

    added

--- src/pages/auth/LogIn.tsx
+++ src/pages/auth/LogIn.tsx
@@ -1,27 +1,55 @@
 import styled from "styled-components"
-import bg from "../../assets/about-img-3.jpg"
-import { NavLink } from "react-router-dom"
+import logo from "../../assets/EChild.png" 
+import { Link } from "react-router-dom"
+import bg from "../../assets/image-3.jpg"
+import { BsFacebook, BsGoogle, BsTwitter } from "react-icons/bs"
 
 const LogIn = () => {
   return (
     <>
-      <Container>
+       <Container>
+        <Left>
+          <Img src={logo}/>
+          <p>Login to your account</p>
+          <Sign>
+          <h3>Don't have an account?</h3>
+          <Link to={"/register"} style={{textDecoration:"none"}}>
+          <h4>Sign Up Free!</h4>
+          </Link>
+          </Sign>
+          <BoxHolder>
+            <Facebook>
+              <BsFacebook size={20} style={{color:"white"}}/>
+            </Facebook>
+            <Twitter>
+              <BsTwitter size={20} style={{color:"white"}}/>
+            </Twitter>
+            <Google>
+              <BsGoogle size={20} style={{color:"white"}}/>
+            </Google>
+          </BoxHolder>
+          <LineHolder>
+            <Line></Line>
+            <h1>or</h1>
+            <Line></Line>
+          </LineHolder>
+          <input type="text" placeholder="Email address"/>
+          <input type="text" placeholder="Password"/>
+          <Holder>
+            <Remember>
+              
+              Remember me
+            </Remember>
+            <Forgot>Forgot password?</Forgot>
+          </Holder>
+          <Button>Login with email</Button>
+        </Left>
+        <Right>
           <Cover>
-          <h1>Signin your child</h1>
-          <Box>
-            <p>Email:</p>
-            <input type="text" placeholder="example@gmail.com" required/>
-          </Box>
-          <Box>
-            <p>Password:</p>
-            <input type="text" placeholder="password" required/>
-          </Box>
-          <Button>Signin</Button>
-          <div style={{display:"flex",alignItems:"center",justifyContent:"center",fontSize:"20px",fontWeight:"800",marginTop:"15px"}}>
-            <span>Don't have an account?</span>
-            <NavLink to={"/register"} style={{textDecoration:"none",color:"white",marginLeft:"10px"}}>Register</NavLink>
-          </div>
+            <h1>Welcome to Echld Account</h1>
+            <span>Lorem ipsum dolor sit amet,</span>
           </Cover>
+        </Right>
       </Container>
     </>
   )
@@ -29,63 +57,181 @@ const LogIn = () => {
 
 export default LogIn
 
-const Button = styled.button`
-  width: 160px;
-  height: 40px;
-  border-radius: 5px;
-  margin-top: 10px;
-  border: none;
-  cursor: pointer;
-  margin-left: 250px;
-  font-size: 20px;
-  font-weight: 800;
-  font-family: Comic Sans MS;
 
-  &:hover{
-    background-color: red;
-    color: white;
-  }
+const Forgot = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
 `
 
-const Box = styled.div`
-margin-left: 20px;
+const Remember = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+`
 
-input{
-  outline: none;
-  width: 380px;
-  height: 30px;
-  padding-left: 5px;
-  border-radius: 5px;
-  border: none;
+const Holder = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-around;
+`
+
+const Line = styled.div`
+width: 150px;
+height: 1px;
+background-color: #DDDDDD;
+border-radius: 2px;
+`
+
+const LineHolder = styled.div`
+display: flex;
+align-items: center;
+gap: 10px;
+margin-top: 20px;
+
+h1{
+  margin-bottom: 17px;
+  font-size: 23px;
+  font-weight: 800;
+  font-family: cursive;
+  color: #DDDDDD;
+}
+`
+
+const Google = styled.div`
+width: 95px;
+height: 45px;
+background-color: #C73B38;
+border-radius: 5px;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+const Twitter = styled.div`
+width: 95px;
+height: 45px;
+background-color: #37B2ED;
+border-radius: 5px;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+const Facebook = styled.div`
+width: 95px;
+height: 45px;
+background-color: #4F66A0;
+border-radius: 5px;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
+const BoxHolder = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+width: 70%;
+`
+
+const Sign = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+
+h4{
+  margin-left: 3px;
+}
+
+h3{
+  margin: 0;
+}
+`
+
+const Button = styled.button`
+width: 330px;
+height: 45px;
+background-color: #4972F2;
+display: flex;
+align-items: center;
+justify-content: center;
+border: none;
+color: white;
+font-size: 20px;
+border-radius: 5px;
+margin-top: 8px;
+cursor: pointer;
+`
+const Img = styled.img`
+height: 150px;
+`
+const Left = styled.div`
+width: 35%;
+height: 100%;
+background-color: #fff;
+display: flex;
+align-items: center;
+flex-direction: column;
+cursor: pointer;
+
+span{
+  font-size: 13px;
+  font-weight: 800;
+  margin-top: 5px;
 }
 
 p{
-  margin: 0;
-  font-size: 20px;
+  font-size: 30px;
   font-weight: 800;
-  font-family: Comic Sans MS;
+  margin: 0;
+}
+
+input{
+  width: 310px;
+  height: 45px;
+  margin: 10px 0px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  padding-left: 20px;
+  font-size: 18px;
+  font-weight: 400;
+  border-radius: 3px;
+  border: 1px solid #DDDDDD;
+  outline: none;
+  cursor: pointer;
+
+  &:hover{
+    box-shadow: 1px 1px 1px 2px  #5572F3;
+  }
 }
 `
-
 const Cover = styled.div`
 width: 100%;
-height: 100vh;
-background-color: rgba(0,0,0,0.3);
+height: 100%;
+background-color: rgba(0,0,0,0.8);
 display: flex;
 align-items: center;
 justify-content: center;
 flex-direction: column;
 color: white;
-`
 
-const Container = styled.div`
-width: 100%;
-height: 100vh;
-background-image:url(${bg});
+h1{
+  margin: 0;
+}
+span{
+  margin-top: 10px;
+}
+`
+const Right = styled.div`
+width: 65%;
+height: 100%;
+background-image: url(${bg});
 background-position: center;
 background-repeat: no-repeat;
 background-size: cover;
+`
+const Container = styled.div`
+width: 100%;
+height: 100vh;
 display: flex;
 align-items: center;
-justify-content: center;
+justify-content: space-between;
 `
